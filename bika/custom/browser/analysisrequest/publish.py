97d1b05a889e4b655088508a5b41aa4d0712a281
ibottosso <ibottosso@gmail.com>
2015-08-06 16:43:41 -0300

    Adicionado metodo

--- bika/custom/browser/analysisrequest/publish.py
+++ bika/custom/browser/analysisrequest/publish.py
@@ -8,9 +8,10 @@ class AnalysisRequestPublishView(_AnalysisRequestPublishView):
 
     def __call__(self):
         return super(AnalysisRequestPublishView, self).__call__()
+
     def get_PropostaComercial(self, ar):
         return ar.Schema().getField('PropostaComercial').get(ar) \
             if 'PropostaComercial' in ar.Schema() else None
-    def get_Method(self, ar):
-        return ar.Schema().getField('Analyses').getField('Method') \
-            if 'Analyses' in ar.Schema() else None
+    def get_Method(self,analise):
+        return analise.Schema().getField('Method').get(analise) \
+            if 'Method' in analise.Schema() else None
